d095675d1c4edab8f06270470d5a0b8b036d478c
cgavril <cristiana.gavril@mvise.de>
2016-11-23 15:40:51 +0100

    restoring unit test approach suitable for and action which enit data/end or error/end.

--- spec/actions/createEvent.spec.js
+++ spec/actions/createEvent.spec.js
@@ -3,7 +3,6 @@ describe('Outlook Create Event', function () {
 
   const nock = require('nock');
   const action = require('../../lib/actions/createEvent');
-  const baseCF = require('../../lib/baseComponentFunction');
 
   const cfg = require('../data/configuration.in.json');
   const jsonIn = require('../data/createEvent_test.in.json');
@@ -14,131 +13,132 @@ describe('Outlook Create Event', function () {
   const microsoftGraphUri = 'https://graph.microsoft.com/v1.0';
   const microsoftGraphApi = `/me/calendars/${cfg.calendarId}/events`;
 
-
   var self;
   beforeEach(function() {
     self = jasmine.createSpyObj('self', ['emit']);
   });
 
-/*
-  it('should emit (data and end events on success create request - case: http 200', function (done) {
-    const scope1 = nock(refreshTokenUri)
+  it('should emit (data and end events on success create request - case: http 200', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
       .reply(200, {access_token: 1});
 
-    const scope2 = nock(microsoftGraphUri)
+    nock(microsoftGraphUri)
       .post(microsoftGraphApi)
       .reply(200, jsonOut);
 
-    action.process.call(self, {body: jsonIn}, cfg, {})
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    runs(function(){
+      action.process.call(self, {body: jsonIn}, cfg, {});
+    });
 
-    function checkResults() {
+    waitsFor(function(){
+      return self.emit.calls.length;
+    });
+
+    runs(function(){
       let calls = self.emit.calls;
       expect(calls.length).toEqual(2);
       expect(calls[0].args[0]).toEqual('data');
       expect(calls[0].args[1].body).toEqual({"id":"testid12345"});
       expect(calls[1].args[0]).toEqual('end');
-      expect(scope1).toBeTruthy();
-      expect(scope2).toBeTruthy();
-    }
+    });
   });
 
-  it('should emit (data and end events on success create request - case: http 201', function (done) {
-    const scope1 = nock(refreshTokenUri)
+  it('should emit (data and end events on success create request - case: http 201', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
       .reply(200, {access_token: 1})
 
-    const scope2 = nock(microsoftGraphUri)
+    nock(microsoftGraphUri)
       .post(microsoftGraphApi)
       .reply(201, jsonOut);
 
-    action.process.call(self, {body: jsonIn}, cfg, {})
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    runs(function(){
+      action.process.call(self, {body: jsonIn}, cfg, {});
+    });
 
-    function checkResults() {
+    waitsFor(function(){
+      return self.emit.calls.length;
+    });
+
+    runs(function(){
       let calls = self.emit.calls;
       expect(calls.length).toEqual(2);
       expect(calls[0].args[0]).toEqual('data');
       expect(calls[0].args[1].body).toEqual({"id":"testid12345"});
       expect(calls[1].args[0]).toEqual('end');
-      expect(scope1).toBeTruthy();
-      expect(scope2).toBeTruthy();
-    }
+    });
   });
 
-  it('should emit error and end events on unsuccessful refresh token request', function (done) {
-    const scope1 = nock(refreshTokenUri)
+  it('should emit error and end events on unsuccessful refresh token request', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
       .reply(401, {access_token: 1});
 
-    action.process.call(self, {body: jsonIn}, cfg, {})
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    runs(function(){
+      action.process.call(self, {body: jsonIn}, cfg, {});
+    });
 
-    function checkResults() {
+    waitsFor(function(){
+      return self.emit.calls.length;
+    });
+
+    runs(function(){
       let calls = self.emit.calls;
       expect(calls.length).toEqual(2);
       expect(calls[0].args[0]).toEqual('error');
       expect(calls[1].args[0]).toEqual('end');
-      expect(scope1).toBeTruthy();
-    }
+    });
   });
 
-  it('should emit error and end events on unsuccessful create request - case: bad request', function (done) {
-    const scope1 = nock(refreshTokenUri)
+  it('should emit error and end events on unsuccessful create request - case: bad request', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
       .reply(200, {access_token: 1});
 
-    const scope2 = nock(microsoftGraphUri)
+    nock(microsoftGraphUri)
       .post(microsoftGraphApi)
       .reply(400, jsonOut);
 
-    action.process.call(self, {body: jsonIn}, cfg, {})
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    runs(function(){
+      action.process.call(self, {body: jsonIn}, cfg, {});
+    });
 
-    function checkResults() {
+    waitsFor(function(){
+      return self.emit.calls.length;
+    });
+
+    runs(function(){
       let calls = self.emit.calls;
       expect(calls.length).toEqual(2);
       expect(calls[0].args[0]).toEqual('error');
       expect(calls[1].args[0]).toEqual('end');
-      expect(scope1).toBeTruthy();
-      expect(scope2).toBeTruthy();
-    }
+    });
   });
 
-  it('should emit error and end events on unsuccessful create request - case: consent problems', function (done) {
-
-   const scope1 = nock(refreshTokenUri)
+  it('should emit error and end events on unsuccessful create request - case: consent problems', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
       .reply(200, {access_token: 1});
 
-   const scope2 = nock(microsoftGraphUri)
+    nock(microsoftGraphUri)
       .post(microsoftGraphApi)
       .reply(403, jsonOut);
 
-   action.process.call(self, {body: jsonIn}, cfg, {})
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    runs(function(){
+      action.process.call(self, {body: jsonIn}, cfg, {});
+    });
 
-   function checkResults() {
+    waitsFor(function(){
+      return self.emit.calls.length;
+    });
+
+    runs(function(){
       let calls = self.emit.calls;
       expect(calls.length).toEqual(2);
       expect(calls[0].args[0]).toEqual('error');
       expect(calls[1].args[0]).toEqual('end');
-      expect(scope1).toBeTruthy();
-      expect(scope2).toBeTruthy();
-    }
-
+    });
   });
-*/
 
 });
