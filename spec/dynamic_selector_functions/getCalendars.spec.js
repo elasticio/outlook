d9f134cbbfa6c5c407517e795fe7484f2896fdf8
cgavril <cristiana.gavril@mvise.de>
2016-11-23 15:54:35 +0100

    restoring old unit test approach

--- spec/dynamic_selector_functions/getCalendars.spec.js
+++ spec/dynamic_selector_functions/getCalendars.spec.js
@@ -16,52 +16,48 @@ describe('Outlook Get Calendars', function () {
     cb = jasmine.createSpy('cb');
   });
 
-  /*
-  it('should return calendar info on success get request', function (done) {
-    const scope1 = nock(refreshTokenUri)
+  it('should return calendar info on success get request', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
-      .reply(200, {access_token: 1});
+      .reply(200, {access_token: 1})
 
-    const scope2 = nock(microsoftGraphUri)
+    nock(microsoftGraphUri)
       .get(microsoftGraphApi)
       .reply(200, jsonOut);
 
-    action.getCalendars(cfg, cb)
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    action.getCalendars(cfg, cb);
 
-    function checkResults() {
+    waitsFor(function () {
+      return cb.callCount;
+    });
+
+    runs(function () {
       expect(cb).toHaveBeenCalled();
       expect(cb.calls.length).toEqual(1);
       expect(cb).toHaveBeenCalledWith(null, { "AAMkAGI2TGuLAAA=" : "Calendar" });
-      expect(scope1).toBeTruthy();
-      expect(scope2).toBeTruthy();
-    }
+    });
   });
 
-  it('should return errors on refresh token failure ', function (done) {
-    const scope1 = nock(refreshTokenUri)
+  it('should return errors on refresh token failure ', function () {
+    nock(refreshTokenUri)
       .post(refreshTokenApi)
-      .reply(401, {access_token: 1});
+      .reply(401, {access_token: 1})
 
-    const scope2 = nock(microsoftGraphUri)
+    nock(microsoftGraphUri)
       .get(microsoftGraphApi)
       .reply(200, jsonOut);
 
-    action.getCalendars(cfg, cb)
-      .then(checkResults)
-      .then(done)
-      .catch(done);
+    action.getCalendars(cfg, cb);
 
-    function checkResults() {
+    waitsFor(function () {
+      return cb.callCount;
+    });
+
+    runs(function () {
       expect(cb).toHaveBeenCalled();
       expect(cb.calls.length).toEqual(1);
       expect(cb).toHaveBeenCalledWith({ statusCode : 401 });
-      expect(scope1).toBeTruthy();
-      expect(scope2).toBeTruthy();
-    }
+    });
   });
-  */
 
 });
