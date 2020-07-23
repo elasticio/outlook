c950fa55daf3cd33dda9bb896de76485f2ad7548
Olha Virolainen <olga.shukel@gmail.com>
2020-07-23 17:24:56 +0300

    add startTime

--- spec-integration/triggers/readMail.spec.js
+++ spec-integration/triggers/readMail.spec.js
@@ -21,7 +21,7 @@ describe('Outlook Read Mail', () => {
         scope: 'openid User.Read Contacts.Read profile Calendars.ReadWrite Mail.ReadWrite Files.ReadWrite.All',
         expires_in: 3600,
         ext_expires_in: 3600,
-        tokenExpiryTime: new Date(new Date().getTime() + 10000),
+        tokenExpiryTime: (new Date('2995-12-17T03:24:00')).toISOString(),
         client_id: process.env.OAUTH_CLIENT_ID,
         client_secret: process.env.OAUTH_CLIENT_SECRET,
         access_token: process.env.ACCESS_TOKEN,
@@ -38,10 +38,9 @@ describe('Outlook Read Mail', () => {
   it('process test', async () => {
     await trigger.process.call(self, {}, cfg, {});
     const { callCount, args } = self.emit;
-    expect(callCount).to.be.eql(3);
     expect(args[0][0]).to.be.eql('data');
-    expect(args[0][1].body.length).to.not.eql(0);
-    expect(args[1][0]).to.be.eql('snapshot');
-    expect(args[2][0]).to.be.eql('end');
+    expect(args[0][1].body.id).to.not.eql(0);
+    expect(args[callCount - 2][0]).to.be.eql('snapshot');
+    expect(args[callCount - 1][0]).to.be.eql('end');
   });
 });
