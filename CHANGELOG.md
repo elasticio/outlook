e7132adcd3cd565aceccd7fe9eb986244c751608
Olha Virolainen <olga.shukel@gmail.com>
2020-07-23 11:21:38 +0300

    minor fix after review

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
 # 1.0.0 (August 5, 2020)
 
-* Revitalize component
 * Use this.logger functionality instead of console.log
 * Update libs to latest versions
 * Update sailor to 2.6.14 version
+* Add `Read Mail` trigger
 
 # 0.0.3 (July 6, 2017)
 
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,13 +1,13 @@
 const { Client } = require('./lib/Client');
 
 module.exports = async function verifyCredentials(credentials) {
-  this.logger.info('Credentials passed for verification %j', credentials);
+  this.logger.trace('Credentials passed for verification %j', credentials);
   const client = new Client(this, credentials);
   try {
     const userInfo = await client.getUserInfo();
     this.logger.info('Found user: %j', userInfo);
   } catch (e) {
-    this.logger.error('Cannot retrieve user information. Credentials is invalid, please check it');
+    this.logger.error('Cannot retrieve user information. Credentials are invalid, please check them');
     throw e;
   }
   return { verified: true };
