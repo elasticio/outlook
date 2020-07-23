d353caf40c137c05401d8a515d6b4bf72001edd4
Olha Virolainen <olga.shukel@gmail.com>
2020-07-23 13:44:02 +0300

    getFolder test added

---
+++ spec-integration/triggers/readMail.spec.js
@@ -0,0 +1,37 @@
+const { Logger } = require('@elastic.io/component-commons-library');
+const chai = require('chai');
+const sinon = require('sinon');
+require('dotenv').config();
+
+const { expect } = chai;
+const logger = Logger.getLogger();
+const trigger = require('../../lib/triggers/readMail');
+
+describe('Outlook Read Mail', () => {
+  let self;
+  let cfg;
+  beforeEach(() => {
+    self = {
+      emit: sinon.spy(),
+      logger,
+    };
+    cfg = {
+      oauth2: {
+        token_type: 'Bearer',
+        scope: 'openid User.Read Contacts.Read profile Calendars.ReadWrite Mail.ReadWrite Files.ReadWrite.All',
+        expires_in: 3600,
+        ext_expires_in: 3600,
+        tokenExpiryTime: new Date(new Date().getTime() + 10000),
+        client_id: process.env.OAUTH_CLIENT_ID,
+        client_secret: process.env.OAUTH_CLIENT_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+      },
+      folderId: process.env.FOLDER_ID,
+    };
+  });
+
+  it('getFolder test', async () => {
+    const result = await trigger.getFolders.call(self, cfg);
+    expect(result).to.not.eql({});
+  });
+});
