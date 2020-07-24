285a318684b29b9b677bce51c2f2f53c0592b4b1
Olha Virolainen <olga.virolainen@elastic.io>
2020-07-24 18:23:59 +0300

    Change read mail trigger (#30)
    * rename readMail trigger, add emitBehavior, add checkbox pollOnlyUnreadMail

--- spec/triggers/readMail.spec.js
+++ spec/triggers/readMail.spec.js
@@ -62,6 +62,71 @@ describe('Outlook Read Mail', () => {
     expect(scope2.isDone()).to.eql(true);
   });
 
+  it('should poll only unread message', async () => {
+    cfg.pollOnlyUnreadMail = true;
+    const scope1 = nock(refreshTokenUri).post(refreshTokenApi)
+      .reply(200, {
+        access_token: 1,
+        expires_in: 3600,
+      });
+
+    const scope2 = nock(microsoftGraphUri).get(microsoftGraphApi)
+      .query({
+        $orderby: 'lastModifiedDateTime asc',
+        $top: 1000,
+        $filter: 'lastModifiedDateTime gt 1970-01-01T00:00:00.000Z and isRead eq false',
+      })
+      .reply(200, jsonOut);
+
+    await trigger.process.call(self, {}, cfg, {});
+    const { callCount, args } = self.emit;
+    expect(callCount).to.eql(5);
+    expect(args[0][0]).to.eql('updateKeys');
+    expect(args[1][0]).to.eql('data');
+    expect(args[1][1].body).to.eql(jsonOut.value[0]);
+    expect(args[2][0]).to.eql('data');
+    expect(args[2][1].body).to.eql(jsonOut.value[1]);
+    expect(args[3][0]).to.eql('snapshot');
+    expect(args[3][1]).to.eql({
+      lastModifiedDateTime: '2020-07-20T11:44:53.999Z',
+    });
+    expect(args[4][0]).to.eql('end');
+    expect(scope1.isDone()).to.eql(true);
+    expect(scope2.isDone()).to.eql(true);
+  });
+
+  it('should poll only unread message and emit all', async () => {
+    cfg.pollOnlyUnreadMail = true;
+    cfg.emitBehavior = 'emitAll';
+    const scope1 = nock(refreshTokenUri).post(refreshTokenApi)
+      .reply(200, {
+        access_token: 1,
+        expires_in: 3600,
+      });
+
+    const scope2 = nock(microsoftGraphUri).get(microsoftGraphApi)
+      .query({
+        $orderby: 'lastModifiedDateTime asc',
+        $top: 1000,
+        $filter: 'lastModifiedDateTime gt 1970-01-01T00:00:00.000Z and isRead eq false',
+      })
+      .reply(200, jsonOut);
+
+    await trigger.process.call(self, {}, cfg, {});
+    const { callCount, args } = self.emit;
+    expect(callCount).to.eql(4);
+    expect(args[0][0]).to.eql('updateKeys');
+    expect(args[1][0]).to.eql('data');
+    expect(args[1][1].body).to.eql({ results: jsonOut.value });
+    expect(args[2][0]).to.eql('snapshot');
+    expect(args[2][1]).to.eql({
+      lastModifiedDateTime: '2020-07-20T11:44:53.999Z',
+    });
+    expect(args[3][0]).to.eql('end');
+    expect(scope1.isDone()).to.eql(true);
+    expect(scope2.isDone()).to.eql(true);
+  });
+
   it('should emit error on unsuccessful refresh token request', (done) => {
     const scope1 = nock(refreshTokenUri).post(refreshTokenApi)
       .reply(401);
