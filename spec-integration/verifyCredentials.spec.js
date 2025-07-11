a87ef28fc9268608a759300853cb1f0839e8ec72
Pavlo Voropaiev <pavel.voropaiev@elastic.io>
2025-07-11 14:08:59 +0300

    Enhance error logging

--- spec-integration/verifyCredentials.spec.js
+++ spec-integration/verifyCredentials.spec.js
@@ -1,6 +1,9 @@
 const { Logger } = require('@elastic.io/component-commons-library');
 const sinon = require('sinon');
 require('dotenv').config();
+const chai = require('chai');
+
+const { expect } = chai;
 
 const logger = Logger.getLogger();
 const verify = require('../verifyCredentials');
@@ -14,7 +17,8 @@ describe('Outlook Verify credentials', () => {
       logger,
     };
     cfg = {
-      oauth2: {
+      secretId: process.env.ELASTICIO_SECRET_ID,
+      oauth: {
         token_type: 'Bearer',
         scope: 'openid User.Read Contacts.Read profile Calendars.ReadWrite Mail.ReadWrite Files.ReadWrite.All',
         expires_in: 3600,
@@ -22,13 +26,14 @@ describe('Outlook Verify credentials', () => {
         tokenExpiryTime: (new Date('2995-12-17T03:24:00')).toISOString(),
         client_id: process.env.OAUTH_CLIENT_ID,
         client_secret: process.env.OAUTH_CLIENT_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
+        // access_token: process.env.ACCESS_TOKEN,
       },
       folderId: process.env.FOLDER_ID,
     };
   });
 
-  it('Verify credentials', async () => {
-    await verify.call(self, cfg);
+  it.only('Verify credentials', async () => {
+    const result = await verify.call(self, cfg);
+    expect(result.verified).to.be.equal(true);
   });
 });
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -2,8 +2,14 @@ const { OutlookClient } = require('./lib/OutlookClient');
 
 module.exports = async function verifyCredentials(cfg) {
   this.logger.info('Verify Credentials started');
-  const client = new OutlookClient(this, cfg);
-  await client.getUserInfo();
-  this.logger.info('User information is retrieved, credentials are valid');
-  return { verified: true };
+  try {
+    const client = new OutlookClient(this, cfg);
+    await client.getUserInfo();
+    this.logger.info('User information is retrieved, credentials are valid');
+    return { verified: true };
+  } catch (err) {
+    this.logger.error('Verify credentials failed');
+    this.logger.error(err);
+    return { verified: false };
+  }
 };
